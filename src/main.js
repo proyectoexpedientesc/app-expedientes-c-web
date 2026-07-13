35ec35015f13197bd3be68ca3ecaccf8e7401b9d
proyectoexpedientesc <proyecto.expedientes.c@gmail.com>
2026-07-13 17:12:18 -0400

    Se agrega herramienta de analitica

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 // main.js
-import 'bulma/css/bulma.css' // 1. Bulma DEBE ir primero
-import './assets/main.css'   // 2. Tus estilos DEBEN ir después para sobreescribir a Bulma
+import 'bulma/css/bulma.css' 
+import './assets/main.css' 
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// 1. Cambiamos la importación por la versión universal para Vue
+import { inject } from "@vercel/analytics"
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+// 2. Ejecutamos la función de inyección después de montar la app
+inject()
